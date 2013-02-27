a5099686bf0a4bd3ad8ad1097a906bfe8ed0209a
mhgrove <mike@clarkparsia.com>
2013-02-27 10:30:31 -0500

    * basic full test suite for airline, runs as a separate program, so no love from the IDE

---
+++ src/test/java/io/airlift/command/AllTests.java
@@ -0,0 +1,31 @@
+package io.airlift.command;
+
+import org.testng.TestNG;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * <p></p>
+ *
+ * @author  Michael Grove
+ * @since   0.6
+ * @version 0.6
+ */
+public class AllTests {
+
+    @BeforeSuite
+    public void testSuite() {
+        TestNG aTestNG = new TestNG();
+
+        aTestNG.setTestClasses(new Class[] { CommandTest.class, io.airlift.command.CommandTest.class, ParametersDelegateTest.class, HelpTest.class, GitTest.class, GalaxyCommandLineParser.class});
+
+        aTestNG.run();
+
+        if (aTestNG.hasFailure()) {
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        new AllTests().testSuite();
+    }
+}
