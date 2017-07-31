1d1cc10d21884653504fe479f110bcd465163e3b
Daniel del Hoyo <daniel.delhoyo@schibsted.com>
2017-07-31 10:23:36 +0200

    Use streams instead of Iterator.transform()

--- src/main/java/io/airlift/airline/GlobalUsageSummary.java
+++ src/main/java/io/airlift/airline/GlobalUsageSummary.java
@@ -4,7 +4,6 @@ import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import io.airlift.airline.model.CommandGroupMetadata;
 import io.airlift.airline.model.CommandMetadata;
 import io.airlift.airline.model.GlobalMetadata;
@@ -12,12 +11,12 @@ import io.airlift.airline.model.OptionMetadata;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newTreeMap;
 import static io.airlift.airline.UsageHelper.toUsage;
+import static java.util.stream.Collectors.toList;
 
 public class GlobalUsageSummary
 {
@@ -93,13 +92,9 @@ public class GlobalUsageSummary
         }
 
         out.append("The most commonly used ").append(global.getName()).append(" commands are:").newline();
-        out.newIndentedPrinter(4).appendTable(Iterables.transform(commands.entrySet(), new Function<Entry<String, String>, Iterable<String>>()
-        {
-            public Iterable<String> apply(Entry<String, String> entry)
-            {
-                return ImmutableList.of(entry.getKey(), firstNonNull(entry.getValue(), ""));
-            }
-        }));
+        out.newIndentedPrinter(4).appendTable(commands.entrySet().stream()
+                .map(entry -> ImmutableList.of(entry.getKey(), firstNonNull(entry.getValue(), "")))
+                .collect(toList()));
         out.newline();
         out.append("See").append("'" + global.getName()).append("help <command>' for more information on a specific command.").newline();
     }
